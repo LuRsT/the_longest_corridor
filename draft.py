e6499155dd638add2c813a1d43146031d25025ea
Gil Gonçalves <lursty@gmail.com>
2018-11-08 21:13:40 +0000

    Add first names for heroes and wrestler names for enemies

--- draft.py
+++ draft.py
@@ -1,26 +1,13 @@
 import random
 
-import tracery
-from tracery.modifiers import base_english
-
 from combat import fight
 from models.characters import Character
 from models.items import Item
 from models.weapons import WEAPONS
+from text import get_corpora, get_str_from_rules
 
 
-def get_str_from_rules(rules, part):
-    grammar = tracery.Grammar(rules)
-    grammar.add_modifiers(base_english)
-    return grammar.flatten(part)
-
-
-def get_corpora(corpora_name):
-    with open(f"corpora/{corpora_name}.txt", "r") as corpora_file:
-        return [n.strip() for n in corpora_file.readlines()]
-
-
-def write_intro(item):
+def intro(item):
     rules = {
         "introduction": "For years the corridor remained unexplored, until it was found out, that it held the precious '#item#', from them on, countless adventurers ventured into the depths of the corridor, looking for fame and fortune.",
         "item": str(item),
@@ -34,46 +21,67 @@ def create_item():
     )
 
 
+def create_character():
+    return Character(
+        random.choice(get_corpora("first_names")),
+        "Human",
+        WEAPONS["magic_sword"],
+        10,
+        10,
+        10,
+    )
+
+
+def create_enemy():
+    return Character(
+        random.choice(get_corpora("enemy_names")), "Orc", WEAPONS["sword"], 10, 10, 10
+    )
+
+
 def create_corridor(item):
-    monsters = [
-        Character("Ulfric", "Orc", WEAPONS["sword"], 10, 10, 10) for _ in range(5)
-    ]
+    monsters = [create_enemy() for _ in range(5)]
 
     corridor = [*monsters, item]
     corridor_history = "The corridor was created by a God who laid out some creatures"
     return corridor[::-1], corridor_history
 
 
-def create_character():
-    return Character("Arnold", "Human", WEAPONS["magic_sword"], 10, 10, 10)
-
-
 def run_corridor(corridor):
-    while len(corridor) > 0:
+    for _ in range(10):
         character = create_character()
-        print(
-            f"{character.name} slowly steps into the dark corridor to start their walk..."
-        )
 
-        challenge = corridor.pop()
+        while len(corridor) > 0:
+            print(
+                f"{character.name} slowly steps into the dark corridor to start their walk..."
+            )
 
-        if isinstance(challenge, Character):
-            enemy = challenge
-            character, enemy = fight(character, enemy)
+            challenge = corridor.pop()
 
-            if not character.is_alive:
-                print(f"{character.name} dies")
-                break
+            if isinstance(challenge, Character):
+                enemy = challenge
+                character, enemy = fight(character, enemy)
+
+                if not character.is_alive:
+                    print(f"{character.name} dies")
+                    break
+                else:
+                    print(f"{enemy.name} dies")
+                    print(f"{character.name} sighs in relief and continues...")
+
+            elif isinstance(challenge, Item):
+                print(f"{character.name} picks up the '{challenge}' triumphantly")
+                return True
+
             else:
-                print(f"{enemy.name} dies")
-                print(f"{character.name} sighs in relief and continues...")
+                print("nothing happened :(")
 
-        elif isinstance(challenge, Item):
-            print(f"{character.name} picks up the '{challenge}' trimphantly")
-            return True
 
-        else:
-            print("nothing happened :(")
+def epilogue(result):
+    if result:
+        print("The corridor started crumbling and was reduced to dust")
+
+    else:
+        print("The corridor remained unbeaten")
 
 
 def main():
@@ -87,19 +95,21 @@ def main():
     """
 
     item = create_item()
-    print(write_intro(item))
+    print(intro(item))
 
     corridor, corridor_history = create_corridor(item)
     print(corridor_history)
 
     result = run_corridor(corridor)
 
-    if result:
-        print("The corridor started crumbling and was reduced to dust")
-
-    else:
-        print("The corridor remained unbeaten")
+    epilogue(result)
 
 
 if __name__ == "__main__":
     main()
+
+    # from combat import simulate_fight
+    # simulate_fight(
+    #     Character("A", "Human", WEAPONS["sword"], 30, 10, 10),
+    #     Character("B", "Orc", WEAPONS["halberd"], 10, 10, 10),
+    # )
